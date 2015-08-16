36670b057cf26b2bce10b9aba81ab5d82ec3dd3c
Corey Quillen <corey.quillen@gmail.com>
2015-08-16 00:03:43 -0500

    remove extraneous bower dependencies

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'ui.router',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
+])
+/*
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+*/
